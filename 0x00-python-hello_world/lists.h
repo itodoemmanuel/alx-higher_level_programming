cf8a2781bce72e6737479a6c73a60dcdcd00d441
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2022-11-28 08:04:51 -0800

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,9 +1,13 @@
-i#ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
-
-#include <stdlib.h>
-
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
@@ -15,4 +19,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
